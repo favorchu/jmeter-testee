90ff7808c72e00934418eedabe87717e6487f9c9
Favor Chu <favorchu@gmail.com>
2023-08-26 13:53:43 +0800

    init.

---
+++ src/main/java/com/example/testee/TesteeApplication.java
@@ -0,0 +1,13 @@
+package com.example.testee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testee/TesteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
